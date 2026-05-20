320452b65dcc15b5c2c33d9307e07842cb8c75d1
Daniel Aditya Istyana <danieladityaistyana@gmail.com>
2026-05-20 19:28:04 +0700

    Revert "feat(join): drop min-h-screen to remove empty space below button (MID-91)"
    This reverts commit fc0a2132be99450be1b5d984bae8065dc77554e1.

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -53,7 +53,7 @@ export default function JoinPage() {
   }
 
   return (
-    <main className="relative flex flex-col">
+    <main className="relative min-h-screen flex flex-col">
       <CornerMarks />
       <header className="px-6 pt-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
